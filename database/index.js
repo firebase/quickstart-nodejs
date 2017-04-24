19bc8a99ef073c8f991a928c66dc0172d4c7c364
Nicolas Garnier <nivco.las@gmail.com>
2017-04-25 00:32:32 +0200

    Update index.js

--- database/index.js
+++ database/index.js
@@ -32,7 +32,7 @@ var mailTransport = nodemailer.createTransport('smtps://<user>%40gmail.com:<pass
 // TODO(DEVELOPER): Change the two placeholders below.
 // [START initialize]
 // Initialize the app with a service account, granting admin privileges
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require('path/to/serviceAccountKey.json');
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
