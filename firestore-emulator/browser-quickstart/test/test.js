fac28107267291c3854b1d46f446995276a3fd22
Ryan P. Brewster <RyanPBrewster@gmail.com>
2019-07-30 18:26:14 -0700

    use puppeteer

--- firestore-emulator/browser-quickstart/test/test.js
+++ firestore-emulator/browser-quickstart/test/test.js
@@ -1,7 +1,23 @@
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
+const puppeteer = require('puppeteer');
+const expect = require("chai").expect;
+
+it('works', async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:5000');
+
+  const { before, after } = await page.evaluate(async () => {
+    const chat = document.getElementById("history");
+    const before = chat.innerText;
+    document.getElementById("input").innerText = "Hello, World!";
+    document.getElementById("submit").click();
+    await new Promise(r => setTimeout(r, 100));
+    const after = chat.innerText;
+    return { before, after };
   });
+
+  expect(before).to.be.eql("");
+  expect(after).to.be.eql("Hello, World!");
+
+  await browser.close();
 });
